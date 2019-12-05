859b5a5e38e4fbb583eec130948bf3ae242769a9
tzhuoga <58494286+tzhuoga@users.noreply.github.com>
2019-12-05 16:03:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ notes = { 'c': 261,
           'e': 329, 
           'f': 349, 
           'g': 391, 
-          'gS': 415,
+          'g#': 415,
           'a': 440,
+          'b': 494,
+          'bb': 466,
           'cH': 523,
+          'dH': 587,
+          'dH#':622,
           'eH': 659,
-          'fH': 698 }
+          'fH': 698,
+          'fH#': 740,
+          'gH': 784,
+          'gH#': 831,
+          'aH': 880,
+          'bH': 988,
+          'bHb':932,
+          'cHH':1046,
+          'rest': 0}
 
 part1 = [[500, notes['a']],
         [500, notes['a']],
@@ -32,11 +44,102 @@ part2 = [[500, notes['eH']],
         [500, notes['eH']],
         [350, notes['fH']],
         [150, notes['cH']],
-        [500, notes['gS']],
+        [500, notes['g#']],
         [350, notes['f']],
         [150, notes['cH']],
         [650, notes['a']]]
 
+Mario1 = [[150, notes['eH']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [150, notes['cH']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [300, notes['gH']],
+        [300, notes['rest']],
+        [300, notes['g']]
+        [300, notes['rest']]]
+
+Mario2 = [[300, notes['cH']],
+        [150, notes['rest']],
+        [150, notes['g']],
+        [300, notes['rest']],
+        [300, notes['e']],
+        [150, notes['rest']],
+        [150, notes['a']],
+        [150, notes['rest']],
+        [150, notes['b']],
+        [150, notes['rest']],
+        [150, notes['bb']],
+        [300, notes['a']],
+        
+        [100, notes['g']],
+        [100, notes['e']],
+        [100, notes['gH']],
+        [300, notes['aH']],
+        [150, notes['fH']],
+        [150, notes['gH']],
+        [150, notes['rest']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [150, notes['cH']],
+        [150, notes['dH']],
+        [150, notes['b']],
+        [150, notes['rest']]]
+
+Mario3 = [[300, notes['rest']],
+        [150, notes['gH']],
+        [150, notes['fH#']],
+        [150, notes['fH']],
+        [150, notes['dH#']],
+        [150, notes['rest']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [150, notes['g#']],
+        [150, notes['a']],
+        [150, notes['cH']],
+        [150, notes['rest']],
+        [150, notes['a']],
+        [150, notes['cH']],
+        [150, notes['dH']],
+        
+        [150, notes['rest']],
+        [150, notes['gH']],
+        [150, notes['fH#']],
+        [150, notes['fH']],
+        [150, notes['dH#']],
+        [150, notes['rest']],
+        [150, notes['eH']],
+        [150, notes['rest']],
+        [150, notes['cHH']],
+        [150, notes['rest']],
+        [150, notes['cHH']],
+        [150, notes['cHH']]]
+        [600, notes['rest']]]
+        [150, notes['gH']],
+        [150, notes['fH#']],
+        [150, notes['fH']],
+        [150, notes['dH#']],
+        [150, notes['rest']],
+        [150, notes['eH']],
+        
+        [150, notes['rest']],
+        [150, notes['g#']],
+        [150, notes['a']],
+        [150, notes['b']],
+        [150, notes['rest']],
+        [150, notes['a']],
+        [300, notes['cH']],
+        [150, notes['dH']],
+        [150, notes['cH']],
+        [150, notes['dH#']],
+        [150, notes['rest']],
+        [150, notes['dH']],
+        [300, notes['rest']],
+        
+        [300, notes['cH']]]
 
 if __name__ == '__main__':
     sc = s.Scribbler(DEVICE)
@@ -60,3 +163,11 @@ if __name__ == '__main__':
         sc.play_song(part1)
         time.sleep(0.150)
         sc.play_song(part2)
+
+#add qr function for Mario song 
+    if second_marker == 0:
+        sc.play_song(Mario1)
+        sc.play_song(Mario2)
+        sc.play_song(Mario2)
+        sc.play_song(Mario3)
+        sc.play_song(Mario3)
